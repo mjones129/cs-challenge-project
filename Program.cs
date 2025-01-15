0425d9b8093b3625cf23e5e0f60ac3fab4199c31
Matt <me@mattjones.tech>
2025-01-15 13:59:04 -0500

    cannot implicitly convert string to int

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-Console.WriteLine("Signed integral types:");
-
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+int first = 2;
+string second = "4";
+int result = first + second;
+Console.WriteLine(result);
