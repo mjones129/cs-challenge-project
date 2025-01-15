57f8e061b0dbdd53c276e73a76d32605acd2979a
Matt <me@mattjones.tech>
2025-01-15 14:10:32 -0500

    example of data loss due to explicit conversion

--- Program.cs
+++ Program.cs
@@ -9,8 +9,14 @@
 // decimal myDecimal = myInt; // This implicit conversion is handled automatically because the data type is "widened". This data type is being converted from a type that holds less to a type that is capable of holding more.
 // Console.WriteLine($"decimal: {myDecimal}");
 
-decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-int myInt = (int)myDecimal; //explicit conversion, must be done manually
-Console.WriteLine($"int: {myInt}");
+// int myInt = (int)myDecimal; //explicit conversion, must be done manually
+// Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
