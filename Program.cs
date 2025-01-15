cc40e40427393def7abe92d1f9f318772e629a48
Matt <me@mattjones.tech>
2025-01-15 16:45:29 -0500

    use String.Join to separate characters

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 string value = "abc123";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
-string result = new string(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
 Console.WriteLine(result);
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9e5d63b9aff070f840173ed419b13a73d9aef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1311e55436c84277feb2f27e0bd130da805f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
