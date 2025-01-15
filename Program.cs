36042c47e858d7cb527a5dddcb650542b269f14a
Matt <me@mattjones.tech>
2025-01-15 14:16:01 -0500

    convert string to int using Covert

--- Program.cs
+++ Program.cs
@@ -21,7 +21,12 @@
 // Console.WriteLine($"Decimal: {myDecimal}");
 // Console.WriteLine($"Float  : {myFloat}");
 
-string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second);
-Console.WriteLine(sum);
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
