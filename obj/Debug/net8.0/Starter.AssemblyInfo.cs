be9e5d63b9aff070f840173ed419b13a73d9aef0
Matt <me@mattjones.tech>
2025-01-15 16:40:15 -0500

    finish array resizing module

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4ca4122228b8a9809bee2b6c99e676e76d6562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f33328879d6677f0ec09b8405c9f7f29a72b8ffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
