96cde859e3531995109d4a77ebbf1e5b9cf7547f
Matt <me@mattjones.tech>
2025-01-15 14:07:46 -0500

    explicit conversion must be done manually

--- Program.cs
+++ Program.cs
@@ -3,8 +3,14 @@
 // string result = first + second;
 // Console.WriteLine(result);
 
-int myInt = 3;
-Console.WriteLine($"int: {myInt}");
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
 
-decimal myDecimal = myInt; // This implicit conversion is handled automatically because the data type is "widened". This data type is being converted from a type that holds less to a type that is capable of holding more.
+// decimal myDecimal = myInt; // This implicit conversion is handled automatically because the data type is "widened". This data type is being converted from a type that holds less to a type that is capable of holding more.
+// Console.WriteLine($"decimal: {myDecimal}");
+
+decimal myDecimal = 3.14m;
 Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal; //explicit conversion, must be done manually
+Console.WriteLine($"int: {myInt}");
