f33328879d6677f0ec09b8405c9f7f29a72b8ffb
Matt <me@mattjones.tech>
2025-01-15 16:34:11 -0500

    resize array with Resize() method

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea086fda3931bb8577ce282753a195b920a67eb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4ca4122228b8a9809bee2b6c99e676e76d6562")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
