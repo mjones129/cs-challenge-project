a4a93c38c47f4c031ad968446de7286686b82b25
Matt <me@mattjones.tech>
2025-01-15 21:28:58 -0500

    string padding with an overloaded method

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
-int invoiceNumber = 1201;
-decimal productShares = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
+string input = "Pad this";
+ Console.WriteLine(input.PadLeft(12));
 
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-
-Console.WriteLine($"   Shares: {productShares:N3} Product");
-Console.WriteLine($"     Sub Total: {subtotal:C}");
-Console.WriteLine($"           Tax: {taxPercentage:P2}");
-Console.WriteLine($"     Total Billed: {total:C}");
+ Console.WriteLine(input.PadLeft(12, '-'));
+ Console.WriteLine(input.PadRight(12, '-'));
