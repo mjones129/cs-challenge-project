fb221c94a01c59dd69540fcb0de5da48f6247e44
Matt <me@mattjones.tech>
2025-01-15 21:15:15 -0500

    formatting numbers

--- Program.cs
+++ Program.cs
@@ -1,19 +1,2 @@
-// string first = "Hello";
-// string second = "World";
-// string result = string.Format("{0} {1}!", first, second);
-// Console.WriteLine(result);
-
-// string first = "Hello";
-// string second = "World";
-// Console.WriteLine("{1} {0}!", first, second);
-// Console.WriteLine("{0} {0} {0}!", first, second);
-
-// string first = "Hello";
-// string second = "World";
-// Console.WriteLine($"{first} {second}!");
-// Console.WriteLine($"{second} {first}!");
-// Console.WriteLine($"{first} {first} {first}!");
-
-decimal price = 123.45m;
-int discount = 50;
-Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N4} units");
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e79ce6528c2f1e104fb146205bd75dcac5b9c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020049a3f3c1c5ecac0e6ec29b2a5133acbe2d5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
