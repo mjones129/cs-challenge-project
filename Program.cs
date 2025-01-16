72422c0dbe7808dcb1d7785f16caa4152043d7e0
Matt <me@mattjones.tech>
2025-01-15 21:07:20 -0500

    composite formatting example

--- Program.cs
+++ Program.cs
@@ -1,25 +1,9 @@
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string first = "Hello";
+// string second = "World";
+// string result = string.Format("{0} {1}!", first, second);
+// Console.WriteLine(result);
 
-//create an array of orders separated by commas
-string[] orders = orderStream.Split(',');
-
-//for each of the orders in the stream count the number of characters
-foreach (string order in orders)
-{
-    // Console.WriteLine(order.Length);
-    //if order length is greater than 4, append "- Error" to the order
-    if (order.Length > 4)
-    {
-        Console.WriteLine(order + " - Error");
-    }
-    // if order length is less than 4, append "- Error to the order
-    else if (order.Length < 4)
-    {
-        Console.WriteLine(order + " - Error");
-    }
-    // output the order
-    else
-    {
-        Console.WriteLine(order);
-    }
-}
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4865957ca5406c350556646b0780ee039c032729")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f2b2d99ff69cddad21731d7df4cd47e7bbad58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
