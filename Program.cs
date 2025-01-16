f2e79ce6528c2f1e104fb146205bd75dcac5b9c1
Matt <me@mattjones.tech>
2025-01-15 21:09:24 -0500

    string interpolation example

--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@
 // string result = string.Format("{0} {1}!", first, second);
 // Console.WriteLine(result);
 
+// string first = "Hello";
+// string second = "World";
+// Console.WriteLine("{1} {0}!", first, second);
+// Console.WriteLine("{0} {0} {0}!", first, second);
+
 string first = "Hello";
 string second = "World";
-Console.WriteLine("{1} {0}!", first, second);
-Console.WriteLine("{0} {0} {0}!", first, second);
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
