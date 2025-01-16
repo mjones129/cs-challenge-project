020049a3f3c1c5ecac0e6ec29b2a5133acbe2d5b
Matt <me@mattjones.tech>
2025-01-15 21:12:34 -0500

    formatting and displaying currency

--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@
 // Console.WriteLine("{1} {0}!", first, second);
 // Console.WriteLine("{0} {0} {0}!", first, second);
 
-string first = "Hello";
-string second = "World";
-Console.WriteLine($"{first} {second}!");
-Console.WriteLine($"{second} {first}!");
-Console.WriteLine($"{first} {first} {first}!");
+// string first = "Hello";
+// string second = "World";
+// Console.WriteLine($"{first} {second}!");
+// Console.WriteLine($"{second} {first}!");
+// Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
