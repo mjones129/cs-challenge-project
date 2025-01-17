301e8d797f2b11f44df7affc1f9167b9b9660ba0
Matt <me@mattjones.tech>
2025-01-16 20:50:49 -0500

    using IndexOf() to find the position of a character in a string

--- Program.cs
+++ Program.cs
@@ -1,27 +1,7 @@
-string customerName = "Ms. Barros";
+string message = "Find what is (inside the parentheses)";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
-
-// Your logic here
-Console.WriteLine($"Dear {customerName},\n");
-Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
-Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}.\n");
-Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}.\n");
-
-Console.WriteLine("Here's a quick comparison:\n");
-
-string comparisonMessage = "";
-
-//current product with right padding, current return with right padding, current profit
-comparisonMessage += $"{currentProduct,-20} {currentReturn,8:P2} {currentProfit,16:C}\n";
-//new product with right padding, new return with right padding, new profit
-comparisonMessage += $"{newProduct,-20} {newReturn,8:P2} {newProfit,16:C}\n";
-
-Console.WriteLine(comparisonMessage);
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
